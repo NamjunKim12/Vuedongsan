f94bf4b0a71aeb52d606e972a26dc805d14cfb48
namjunkim12 <wrasf175@naver.com>
2021-06-29 19:32:02 +0900

    vue component,props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<Modal :room='room' :itemNumber = 'itemNumber' modal_open = 'modal_open'/>
+
+<div class="menu">
+  <a v-for="category in menu" :key="category">{{category}}</a>
+</div>
+
+<Discount/>
+
+  <div class='item_container' v-for='(a, i) in room' :key="i" >
+    <div class='item' >
+      <img @click = "modal_open = true; itemNumber = i" :src='a.image' class='thumbnail' alt='썸네일'>
+      <h4 @click = "modal_open = true; itemNumber = i">{{a.title}}</h4>
+      <p>{{a.price}}</p>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import roomData from './assets/oneroom.js';
+import Discount from './components/Discount.vue';
+import Modal from './components/Modal.vue';
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      room : roomData,
+      itemNumber : 0,
+      menu : ['Home','Shop', 'About'],
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+      price : [50, 60, 70],
+      modal_open : false,
+      
+
+    }
+  },
+  methods:{
+
+},
   components: {
-    HelloWorld
+    Discount : Discount,
+    Modal : Modal,
   }
 }
+
 </script>
 
 <style>
@@ -21,6 +57,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+
+.menu {
+  background : darkslateblue;
+  padding : 15px;
+  border-radius : 5px;
+}
+.menu a {
+  color : white;
+  padding : 10px;
+  }
+
+.item{
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+img{
+  width: 450px;
+  height: 250px;
+}
+
+
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="black-bg" v-if="modal_open == true">
+        <div class="white-bg">
+          <h4>{{room[itemNumber].title}}</h4>
+          <img :src='room[itemNumber].image' class='thumbnail' alt='썸네일'>
+          <p>{{room[itemNumber].price}}</p>
+          <p>{{room[itemNumber].content}}</p>
+          <Discount/>
+          <!-- <button @click='modal_open =false'>닫기</button> -->
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name : 'Modal',
+    props : {
+        room : Array,
+        itemNumber : Number,
+        modal_open : Boolean,
+    }
+}
+</script>
+
+<style>
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  transform: translate(50%, 20%);
+  width: 50%; 
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
+
+.white-bg img{
+  width: 80%;
+  height:80%;
+}
+</style>
